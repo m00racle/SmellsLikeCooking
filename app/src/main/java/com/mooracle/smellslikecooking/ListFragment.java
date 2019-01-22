4e44d14c7ace086d35df60321c324c420d35b35d
mooracle <yanuar.prakosa@gmail.com>
2019-01-22 21:19:58 +0700

    Making a List

--- app/src/main/java/com/mooracle/smellslikecooking/ListFragment.java
+++ app/src/main/java/com/mooracle/smellslikecooking/ListFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.zip.Inflater;
 
@@ -20,5 +23,47 @@ public class ListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
         return view;
+    }
+
+    public class ListAdapter extends RecyclerView.Adapter {
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new ListViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            //cast the list view holder into the holder and call the bind view method to pass the position:
+            ((ListViewHolder) holder).bindView(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return Recipes.names.length;
+        }
+
+        private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            private TextView mTextView;
+            private ImageView mImageView;
+
+            public ListViewHolder(@NonNull View itemView) {
+                super(itemView);
+                mImageView = itemView.findViewById(R.id.itemImage);
+                mTextView = itemView.findViewById(R.id.itemText);
+            }
+
+            @Override
+            public void onClick(View v) {
+                itemView.setOnClickListener(this);
+            }
+
+            public void bindView(int position){
+                mTextView.setText(Recipes.names[position]);
+                mImageView.setImageResource(Recipes.resourceIds[position]);
+            }
+        }
     }
 }
