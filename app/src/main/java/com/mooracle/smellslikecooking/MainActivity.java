dbbc3a86270eddf42cb045deb981424a953b1332
mooracle <yanuar.prakosa@gmail.com>
2019-01-22 12:09:41 +0700

    Adding Fragments to Activities

--- app/src/main/java/com/mooracle/smellslikecooking/MainActivity.java
+++ app/src/main/java/com/mooracle/smellslikecooking/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mooracle.smellslikecooking;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // crete the lis fragment object:
+        ListFragment fragment = new ListFragment();
+
+        // create fragment manager to add our fragment into place holder:
+        //this is the latest app support fragment manager since the old getFragmentManager method has beed deprecated
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // create the fragment transaction variable:
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeHolder, fragment);
+        fragmentTransaction.commit();
     }
 }
