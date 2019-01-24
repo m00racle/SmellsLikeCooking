9921a0763a54dd9aefb4fbd8b9e4448acf165618
mooracle <yanuar.prakosa@gmail.com>
2019-01-24 15:38:50 +0700

    Implement New NewListAdapter with generics

--- app/src/main/java/com/mooracle/smellslikecooking/ListFragment.java
+++ app/src/main/java/com/mooracle/smellslikecooking/ListFragment.java
@@ -9,8 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 /**This class is used to represent the fragment_list layout.
  * Since this is a Fragment this class must extends Fragment class*/
@@ -35,7 +33,8 @@ public class ListFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
 
         //add the List Adapter object
-        ListAdapter listAdapter = new ListAdapter(listener);
+        //ListAdapter listAdapter = new ListAdapter(listener);
+        NewListAdapter listAdapter = new NewListAdapter(listener);
 
         //set the List Adapter to recycler view:
         recyclerView.setAdapter(listAdapter);
@@ -46,63 +45,5 @@ public class ListFragment extends Fragment {
         //attach the layput manager to the recycler view
         recyclerView.setLayoutManager(layoutManager);
         return view;
-    }
-
-    public class ListAdapter extends RecyclerView.Adapter {
-
-        private final OnRecipeSelectedInterface mListener;
-
-        public ListAdapter(OnRecipeSelectedInterface listener) {
-            mListener = listener;
-        }
-
-
-
-        @NonNull
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-            return new ListViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-            //cast the list view holder into the holder and call the bind view method to pass the position:
-
-            ((ListViewHolder) holder).mImageView.setImageResource(Recipes.resourceIds[position]);
-            ((ListViewHolder) holder).mTextView.setText(Recipes.names[position]);
-            ((ListViewHolder) holder).setIndex(position);
-        }
-
-        @Override
-        public int getItemCount() {
-            return Recipes.names.length;
-        }
-
-        private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-            private int mIndex;
-            TextView mTextView;
-            ImageView mImageView;
-
-            public ListViewHolder(@NonNull View itemView) {
-                super(itemView);
-                mImageView = itemView.findViewById(R.id.itemImage);
-                mTextView = itemView.findViewById(R.id.itemText);
-
-                //: fix the item view set on click listener constructor:
-                itemView.setOnClickListener(this);
-            }
-
-            //give onClick listeners for mImageView and mTextView which by default non clickable.
-            @Override
-            public void onClick(View v) {
-                mListener.onListRecipeSelected(mIndex);
-            }
-
-            // :set index that will be used in the onClick listener
-            public void setIndex(int index){
-                mIndex = index;
-            }
-        }
     }
 }
