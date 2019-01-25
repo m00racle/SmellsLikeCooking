110aa32c68adcc752d3799aa573bae829e9c798d
mooracle <yanuar.prakosa@gmail.com>
2019-01-25 10:36:19 +0700

    Keeping Instance of Instance State

--- app/src/main/java/com/mooracle/smellslikecooking/MainActivity.java
+++ app/src/main/java/com/mooracle/smellslikecooking/MainActivity.java
@@ -14,17 +14,23 @@ public class MainActivity extends LoggingActivity implements ListFragment.OnReci
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // crete the lis fragment object:
-        ListFragment fragment = new ListFragment();
+        //: create a new field of listFragment as basis for if checking: (Remember to cast it to ListFragment)
+        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.placeHolder);
 
-        // create fragment manager to add our fragment into place holder:
-        //this is the latest app support fragment manager since the old getFragmentManager method has beed deprecated
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        //: if the savedFragment is null then only add new fragment:
+        if (savedFragment == null){// crete the lis fragment object:
+            ListFragment fragment = new ListFragment();
 
-        // create the fragment transaction variable:
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.placeHolder, fragment);
-        fragmentTransaction.commit();
+            // create fragment manager to add our fragment into place holder:
+            //this is the latest app support fragment manager since the old getFragmentManager method has beed deprecated
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            // create the fragment transaction variable:
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.placeHolder, fragment);
+            fragmentTransaction.commit();
+        }
+
     }
 
     @Override
