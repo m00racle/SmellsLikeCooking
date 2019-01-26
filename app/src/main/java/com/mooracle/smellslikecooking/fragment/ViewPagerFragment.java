0d1417a59baee4c6e25ba375185509fa98ada682
mooracle <yanuar.prakosa@gmail.com>
2019-01-26 17:49:39 +0700

    Adding The View Pager

--- app/src/main/java/com/mooracle/smellslikecooking/fragment/ViewPagerFragment.java
+++ app/src/main/java/com/mooracle/smellslikecooking/fragment/ViewPagerFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,26 @@ public class ViewPagerFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //: add a View object named view with layout in use fragment_viewpager, with no attach to root
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        //: set the IngredientFragment and DirectionFragment to be used in the viewPager Adapter:
+        final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
+
+        //: add ViewPager object into this fragment:
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+
+        //: set the ViewPager adapter: we will use the FragmentPagerAdapter and pass getChildFragmentManager
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                if (position == 0){return ingredientsFragment;} else {return directionsFragment;}
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
 
         //: get the int index value from the bundle
         index = getArguments().getInt(KEY_RECIPE_INDEX);
