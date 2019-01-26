6366f3bd2b3e1d450304520d5c47510ce8f83bc4
mooracle <yanuar.prakosa@gmail.com>
2019-01-26 11:58:28 +0700

    Initializing a Fragment with Data

--- app/src/main/java/com/mooracle/smellslikecooking/fragment/ViewPagerFragment.java
+++ app/src/main/java/com/mooracle/smellslikecooking/fragment/ViewPagerFragment.java
@@ -8,13 +8,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.mooracle.smellslikecooking.R;
+import com.mooracle.smellslikecooking.model.Recipes;
 
 public class ViewPagerFragment extends Fragment {
+    //: set tag for ViewPagerFragment:
+    public static final String VIEWPAGER_FRAGMENT = "Viewpager_Fragment";
+
+    //: make String constant name to store the index name:
+    public static final String KEY_RECIPE_INDEX = "recipe_index";
+    private int index;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //: add a View object named view with layout in use fragment_viewpager, with no attach to root
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        //: get the int index value from the bundle
+        index = getArguments().getInt(KEY_RECIPE_INDEX);
+
+        //: add Toast from main activity to this fragment:
+        //Toast.makeText(getActivity(), Recipes.names[index], Toast.LENGTH_SHORT).show();
+        //: this toast will be changed by changing the action bar title into the recipe name
+        getActivity().setTitle(Recipes.names[index]);
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        //: when this fragment ends we need to return the action bar title back to app default (app name)
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
     }
 }
