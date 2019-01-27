0f1a6579082f13d6ac692c9f2ee385a80ed319fb
mooracle <yanuar.prakosa@gmail.com>
2019-01-27 13:05:17 +0700

    The State of CheckBoxes

--- app/src/main/java/com/mooracle/smellslikecooking/fragment/IngredientsFragment.java
+++ app/src/main/java/com/mooracle/smellslikecooking/fragment/IngredientsFragment.java
@@ -15,6 +15,10 @@ import com.mooracle.smellslikecooking.model.Recipes;
 import java.util.Objects;
 
 public class IngredientsFragment extends Fragment {
+    private static final String KEY_CHECKED_BOXES = "key_checked_boxes";
+    // add a checkboxes array to enable access of the checkboxes created from outside:
+    private CheckBox[] checkBoxes;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,24 +30,62 @@ public class IngredientsFragment extends Fragment {
         //: get an array of ingredients using get array and separated using regex " ' ":
         String[] ingredients = Recipes.ingredients[index].split("`");
 
+        //: initialize the array of checkboxes with the length the same as the ingredients array of String:
+        checkBoxes = new CheckBox[ingredients.length];
+
+        //: set an array of boolean to store each stateOfCheckboxes only if there are one in savedInstanceState:
+        boolean[] checkedBoxes = new boolean[checkBoxes.length];
+        if (savedInstanceState != null && savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES) != null){
+            checkedBoxes = savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES);
+        }
+
         //: reference the linear layout in the ingredients_fragment xml:
         LinearLayout linearLayout = view.findViewById(R.id.ingredientLayout);
 
         //: loop through all ingredients and make checkboxes from each of ingredient:
-        setUpCheckBoxes(ingredients, linearLayout);
+        setUpCheckBoxes(ingredients, linearLayout, checkedBoxes);
         return view;
     }
 
-    private void setUpCheckBoxes(String[] ingredients, @Nullable ViewGroup container) {
+    private void setUpCheckBoxes(String[] ingredients, @Nullable ViewGroup container, boolean[] checkedBoxes) {
+        //: set int index called i to denotes the checkbox index instead just new chcekBox object:
+        int i = 0;
+
         for (String ingredient : ingredients){
-            CheckBox checkBox = new CheckBox(getActivity());
-
+            checkBoxes[i] = new CheckBox(getActivity());
             //: set some properties of the checkbox:
-            checkBox.setPadding(8, 16, 8, 16);
-            checkBox.setTextSize(20);
-            checkBox.setText(ingredient);
+            checkBoxes[i].setPadding(8, 16, 8, 16);
+            checkBoxes[i].setTextSize(20);
+            checkBoxes[i].setText(ingredient);
 
-            Objects.requireNonNull(container).addView(checkBox);
+            Objects.requireNonNull(container).addView(checkBoxes[i]);
+
+            //: determine if the checkbox is checked or not: if it is then use toggle() to make it checked
+            if (checkedBoxes[i]){checkBoxes[i].toggle();}
+
+            //: increment the int i to the next index of checkBox in checkBoxes
+            i++;
         }
+    }
+
+    //: create an override of method onSaveInstanceState to save the state of each checkbox:
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        //: make boolean variable to store each state of the checkbox:
+        // NOTE: the length of this boolean array will always the same as checkBoxes array:
+        boolean[] stateOfCheckBoxes = new boolean[checkBoxes.length];
+
+        //: loop through all checkboxes to save the state of them:
+        // NOTE: we use int i as index for each stateOfCheckBoxes
+        int i = 0;
+        for (CheckBox checkBox : checkBoxes){
+            stateOfCheckBoxes[i] = checkBox.isChecked();
+            i++;
+        }
+
+        //: set all stateOfCheckBoxes into a bundle called outState to be save in the super:
+        outState.putBooleanArray(KEY_CHECKED_BOXES, stateOfCheckBoxes);
+
+        super.onSaveInstanceState(outState);
     }
 }
